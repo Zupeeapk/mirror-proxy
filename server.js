b0d232f39b773c47f607c84730cb443b8fbbd56b
Zupee APK <zupee326@gmail.com>
2025-05-01 21:31:17 +0500

    server.js

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ const express = require("express");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
+const target = "https://animixplay.st";
 
 app.use("/", createProxyMiddleware({
-  target: "https://animixplay.st", // change this to your target site
+  target,
   changeOrigin: true,
+  selfHandleResponse: false,
+
+  // Remove compression to allow future HTML rewriting (if needed)
+  onProxyReq: (proxyReq) => {
+    proxyReq.removeHeader("accept-encoding");
+  },
+
+  // Rewrites redirect responses (Location header)
+  onProxyRes: (proxyRes, req, res) => {
+    const locationHeader = proxyRes.headers["location"];
+    if (locationHeader && locationHeader.startsWith(target)) {
+      proxyRes.headers["location"] = locationHeader.replace(
+        target,
+        `${req.protocol}://${req.get("host")}`
+      );
+    }
+  },
+
   pathRewrite: { "^/": "/" }
 }));
 
